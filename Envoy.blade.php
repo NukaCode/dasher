7e0635593b114b0353fc4f72a6fd00f3416aa3b0
Travis Blasingame <stygian.warlock.v2@gmail.com>
2015-06-12 12:57:53 -0500

    Adding get task for nuka installer

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,9 @@
 @servers(['local' => '127.0.0.1'])
 
-@task('nuka-installer')
-    ~/.composer/vendor/bin/laravel --version
+@task('get-nuka-installer')
+    composer global require "nukacode/installer=~2.1"
+@endtask
+
+@task('nuka-installer-version')
     ~/.composer/vendor/bin/laravel --version
 @endtask
